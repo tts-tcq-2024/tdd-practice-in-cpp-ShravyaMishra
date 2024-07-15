ea91c4120b14ea16d7e0535e948127407ac171a1
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-07-15 17:36:31 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,21 @@
+#include "StringCalculator.h"
 
+int StringCalculator::add(const std::string& input) {
+    // Return 0 for empty input
+    if (input.empty()) {
+        return 0;
+    }
+    
+    // Parse the input to get numbers
+    std::vector<int> numbers = parseNumbers(input, ",\n");
+
+    // Sum the numbers, ignoring those greater than 1000
+    int sum = 0;
+    for (int num : numbers) {
+        if (num <= 1000) {
+            sum += num;
+        }
+    }
+
+    return sum;
+}
